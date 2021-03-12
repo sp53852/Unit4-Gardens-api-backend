4baf873d6f8d2f31e725ac8a86daeb40e183be9e
Sharmila Pitta <pittasharmilab@johndeere.com>
2021-03-12 11:03:38 -0600

    backend api restful routes are working

--- controllers/gardensController.js
+++ controllers/gardensController.js
@@ -4,13 +4,49 @@ const router = express.Router();
 const GardenModel = require("../models").Garden;
 const PlantModel = require("../models").Plant;
 
+// SHOW Specific Garden - Postman-GET: http://localhost:3000/api/gardens/profile/1 
+router.get("/profile/:id", async (req, res) => {
+    let garden = await GardenModel.findByPk(req.params.id, {
+        include: [{ model: PlantModel, attributes: ['id', 'name'] }]
+    });
 
+    res.json({ garden });
+});
 
+// INDEX  - Postman-GET: http://localhost:3000/api/gardens
+router.get("/", async (req, res) => {
+    let gardens = await GardenModel.findAll({ include: PlantModel });
+    res.json({ gardens });
+});
 
+// CREATE NEW GARDEN - Postman-POST: http://localhost:3000/api/gardens
+router.post("/", async (req, res) => {
+    let garden = await GardenModel.create(req.body);
+    res.json({ garden });
+});
 
+// UPDATE GARDEN - Postman-PUT: lhttp://localhost:3000/api/gardens/profile/7
+router.put("/:id", async (req, res) => {
+    let garden = await GardenModel.update(req.body, {
+        where: { id: req.params.id },
+        returning: true
+    });
 
+    res.json({ garden });
+});
 
+// DELETE GARDEN - Postman-DELETE: http://localhost:3000/api/gardens/profile/8
+router.delete("/:id", async (req, res) => {
+    await GardenModel.destroy({ where: { id: req.params.id }});
+    res.json({ message: `garden with id ${req.params.id} was deleted` });
+});
 
+// CREATE NEW PLANT - Postman-POST: http://localhost:3000/api/gardens/7/newplant
+router.post("/:id/newplant", async (req, res) => {
+    let garden = await GardenModel.findByPk(req.params.id);
+    let plant = await garden.createPlant(req.body);
+    res.json({ plant });
+})
 
 
 
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();              //app is an object
-//const methodOverride = require("method-override");
+const methodOverride = require("method-override");
+const cors = require("cors");
 
 
 //MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+// app.get('/somedata', (req, res) => {
+//     res.send('here is your information');
+// });
 
 
-app.get('/somedata', (req, res) => {
-    res.send('here is your information');
-});
-
-
-//app.use("/api/gardens", require("./controllers/gardensController.js"));
+app.use("/api/gardens", require("./controllers/gardensController.js"));
 
 // app.listen(3000, ()=>{
 //     console.log("I am listening");
